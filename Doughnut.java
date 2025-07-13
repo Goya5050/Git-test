3ed1ac02881e1d70f42f6dfeb9b74dd8604aecd0
goya <tmoon5050@gmail.com>
2025-07-13 11:39:52 +0900

    Update Doughnut.java

--- Doughnut.java
+++ Doughnut.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class Doughnut {
     public static void main(String[] args) {
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
         Scanner sc = new Scanner(System.in);
         int hight = sc.nextInt();
         int width = sc.nextInt();
